df4b0f8e714b2246a69aab182fb149966af70cc7
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-21 21:31:04 +0000

    Added Notes folder and Primitive and Reference.js file

---
+++ Notes/Primitive and Reference.js
@@ -0,0 +1,9 @@
+// Primitive and Reference Values
+// JavaScript provides six primitive types as undefined, null, boolean, number, string, and symbol.
+// Reference type is object
+
+// Primitive Value
+// When you manipulate the value, you are working on the actual value stored in the variable. 
+// The variable that stores a primitive value is accessed by value
+
+// Reference: A variable that stores an object is accessed by reference.
