70d36f37f5f2c24c6b97f451fb197f54edc8c982
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-22 20:47:02 +0000

    Added Chapter 3.js information

---
+++ Notes/Values vs Reference.js
@@ -0,0 +1,6 @@
+//Use let in for loops
+// This - a variable with the value of the object that invokes the function where this is used.
+// It is determined by how a function is called
+// This is used inside of functions
+// This holds a value of undefined
+// This refers to an object
