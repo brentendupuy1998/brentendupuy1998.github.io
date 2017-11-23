0fde36c965e68d00bd4e884701ab045e1aeb09ec
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-23 17:15:57 +0000

    Fixing Value vs Reference

--- Notes/Values vs Reference.js
+++ Notes/Values vs Reference.js
@@ -2,3 +2,10 @@
 // Refers to an object
 //Left of the dot at call time
 // If nothing is to the left, it will be bound to the global object
+
+'use strict';
+var hello = function() {
+    console.log(this);
+};
+
+//It always has to be an object no matter what
