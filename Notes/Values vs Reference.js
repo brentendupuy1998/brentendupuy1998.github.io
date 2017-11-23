6cbbf49c3bae00094d3c872ec344d6fbdc31d662
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-23 17:12:18 +0000

    Fixing Value vs Reference

--- Notes/Values vs Reference.js
+++ Notes/Values vs Reference.js
@@ -1,6 +1,3 @@
 //Use let in for loops
-// This - a variable with the value of the object that invokes the function where this is used.
-// It is determined by how a function is called
-// This is used inside of functions
-// This holds a value of undefined
-// This refers to an object
+// Refers to an object
+//Left of the dot at call time
