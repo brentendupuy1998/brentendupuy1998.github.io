81f5aa0524188e3e834bf772ef719f9c0286935f
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-29 18:39:16 +0000

    Fixed reduce.js area

--- Notes/Reduce.js
+++ Notes/Reduce.js
@@ -1,2 +1,20 @@
 // What parameters does Reduce take?
 // What parameters does the anonymous function in reduce take?
+// What does reduce return?
+// What does the anonymous function in reduce return?
+
+
+
+/**
+ * Reduce takes three params, collection, function, seed
+ * Loops over a collection
+ * Seed is optional
+ * If seed is not given it, it becomes the first value of collection
+ * func takes 4 parameters: seed, collection[i], i, collection
+ * seed = the result of func
+ * Returns seed
+ * @param {array} to loop over
+ * @param {function}
+ * @param Seed that has a starting point for reduce
+ * @return from the final call
+ */
